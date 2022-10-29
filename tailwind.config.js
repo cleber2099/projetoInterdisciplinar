ed9973881d9a71b0da3f30c022841af8ca94b905
cleber2099 <cleber.santos.2099@gmail.com>
2022-10-29 01:39:41 -0300

    fisrt commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      
+      spacing: {
+        '14': '3.5rem',
+        '125':'125px',
+        '200':'200px',
+      },
+      colors: {
+        'hoverspt':'#18D760',
+        'purple-main':'#2D4689',
+        'green-main':'#1ED760'
+      },
+      backgroundImage: theme => ({
+        'spotify-theme':"url('/img/bursts.svg')",
+        'spotify-theme-mobile':"url('/img/bursts-mobile.svg')",
+
+      }),
+      backgroundSize: {
+        '175%': '175%',
+        '195%': '195%',
+      },
+      backgroundPosition: {
+        'banner':'46% 4%',
+        'banner-mobile':'top 25% center'
+      },
+      fontSize: {
+        '9xl': '9rem'
+      },
+    },
+  },
+  plugins: [],
+}
